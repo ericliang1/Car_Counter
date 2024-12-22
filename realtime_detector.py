c4e0c7701e93447188411292504a1192b94ebec4
Eric Liang <eric.jinchen.liang@gmail.com>
2024-12-21 17:32:46 -0800

    improved formatting

--- realtime_detector.py
+++ realtime_detector.py
@@ -1,32 +1,30 @@
 import time
+
 import cv2
 import torch
 import streamlit as st
 from ultralytics import YOLO
 
-#create real-time detection app
 
-def inference():
+#create real-time detection app
+def run_inference():
 
     #configure settings
-
     st.set_page_config(page_title="Real-Time Television Detector", layout="wide", initial_sidebar_state="auto")
 
-    webcam = 0  
+    WEBCAM = 0  
 
     model = YOLO("runs/detect/train6/weights/best.pt")  
-    class_names = list(model.names.values())
 
+    class_names = list(model.names.values())
     selected_classes = ["television"]
     selected_index = [class_names.index(option) for option in selected_classes]
 
     #set minimum detection thresholds
-
-    conf = 0.7  
-    iou = 0.45
+    CONF_MIN = 0.7  
+    IOU_MIN = 0.45
 
     #create video boxes
-
     col1, col2 = st.columns(2)
     col1.write("**Original**")
     col2.write("**Annotated**")
@@ -39,7 +37,7 @@ def inference():
     #test if start button pressed
 
     if st.sidebar.button("Start"):
-        videocapture = cv2.VideoCapture(webcam) 
+        videocapture = cv2.VideoCapture(WEBCAM) 
 
         if not videocapture.isOpened():
             st.error("Webcam can not be opened")
@@ -55,22 +53,18 @@ def inference():
             prev_time = time.time()  
 
             #find detections
-
-            results = model(frame, conf=conf, iou=iou, classes=selected_index)
+            results = model(frame, conf=CONF_MIN, iou=IOU_MIN, classes=selected_index)
             annotated_frame = results[0].plot()
 
             #record fps
-
             curr_time = time.time()
             fps = 1 / (curr_time - prev_time)
 
             #display original video alongside annotated video
-
             original.image(frame, channels="BGR")
             annotated.image(annotated_frame, channels="BGR")
 
             #test if stop button pressed
-
             if stop_button:
                 videocapture.release()  
                 torch.cuda.empty_cache() 
@@ -84,4 +78,4 @@ def inference():
     print(class_names)
 
 if __name__ == "__main__":
-    inference()
+    run_inference()
--- yolov8_converter.py
+++ yolov8_converter.py
@@ -3,18 +3,17 @@ import os
 import shutil
 
 #define paths
+TRAIN_IMAGES_PATH = "C:/Users/Eric.Jinchen.Liang/fiftyone/coco-2017/train/data"
+TRAIN_LABELS_PATH = "coco_converted/labels/labels"
+VALIDATION_IMAGES_PATH = "C:/Users/Eric.Jinchen.Liang/fiftyone/coco-2017/validation/data"
+VALIDATION_LABELS_PATH = "coco_converted2/labels/labels"
+NEW_TRAIN_IMAGES_PATH = "dataset/images/train"
+NEW_TRAIN_LABELS_PATH = "dataset/labels/train"
+NEW_VALIDATION_IMAGES_PATH = "dataset/images/validation"
+NEW_VALIDATION_LABELS_PATH = "dataset/labels/validation"
 
-train_images_path = "C:/Users/Eric.Jinchen.Liang/fiftyone/coco-2017/train/data"
-train_labels_path = "coco_converted/labels/labels"
-validation_images_path = "C:/Users/Eric.Jinchen.Liang/fiftyone/coco-2017/validation/data"
-validation_labels_path = "coco_converted2/labels/labels"
-new_train_images_path = "dataset/images/train" 
-new_train_labels_path = "dataset/labels/train"  
-new_validation_images_path = "dataset/images/validation"      
-new_validation_labels_path = "dataset/labels/validation"
 
 #convert labels from coco format to yolo format
-
 def convert_files():
     convert_coco(  
         labels_dir="../../../fiftyone/coco-2017/train",
@@ -30,16 +29,16 @@ def convert_files():
         cls91to80=True,
     )
 
-#create dataset directory
 
+#create dataset directory
 def make_directories():
     os.makedirs("dataset/images/train", exist_ok=True)
     os.makedirs("dataset/images/validation", exist_ok=True)
     os.makedirs("dataset/labels/train", exist_ok=True)
     os.makedirs("dataset/labels/validation", exist_ok=True)
 
-#convert labels to usable format
 
+#convert labels to usable format
 def convert_label(path):
     for label_file in os.listdir(path):
         if label_file.endswith(".txt"): 
@@ -55,25 +54,24 @@ def convert_label(path):
             with open(file_path, "w") as file:
                 file.write("\n".join(new))
 
+
 #move data to new dataset directory
-
 def move_data():
-    for file in os.listdir(train_images_path):
-        shutil.copy(os.path.join(train_images_path, file), new_train_images_path)
+    for file in os.listdir(TRAIN_IMAGES_PATH):
+        shutil.copy(os.path.join(TRAIN_IMAGES_PATH, file), NEW_TRAIN_IMAGES_PATH)
 
-    for file in os.listdir(train_labels_path):
-        shutil.copy(os.path.join(train_labels_path, file), new_train_labels_path)
+    for file in os.listdir(TRAIN_LABELS_PATH):
+        shutil.copy(os.path.join(TRAIN_LABELS_PATH, file), NEW_TRAIN_LABELS_PATH)
 
-    for file in os.listdir(validation_images_path):
-        shutil.copy(os.path.join(validation_images_path, file), new_validation_images_path)
+    for file in os.listdir(VALIDATION_IMAGES_PATH):
+        shutil.copy(os.path.join(VALIDATION_IMAGES_PATH, file), NEW_VALIDATION_IMAGES_PATH)
 
-    for file in os.listdir(validation_labels_path):
-        shutil.copy(os.path.join(validation_labels_path, file), new_validation_labels_path)
-
+    for file in os.listdir(VALIDATION_LABELS_PATH):
+        shutil.copy(os.path.join(VALIDATION_LABELS_PATH, file), NEW_VALIDATION_LABELS_PATH)
 
 if __name__ == "__main__":
     convert_files()
     make_directories()
     move_data()
-    convert_label(new_train_labels_path)
-    convert_label(new_validation_labels_path)
+    convert_label(NEW_TRAIN_LABELS_PATH)
+    convert_label(NEW_VALIDATION_LABELS_PATH)
